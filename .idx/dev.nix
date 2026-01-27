6da5b600a36846a9c9ebff267052705fec2ddacf
NGUYỄN ĐỨC TÍN <tktgaming.bg@gmail.com>
2026-01-27 16:13:07 +0000

    update

---
+++ .idx/dev.nix
@@ -0,0 +1,73 @@
+{ pkgs, ... }: {
+  channel = "stable-24.11";
+  packages = [
+    pkgs.docker
+    pkgs.socat
+    pkgs.coreutils
+    pkgs.gnugrep
+    pkgs.sudo
+    pkgs.apt
+    pkgs.docker
+    pkgs.systemd
+    pkgs.unzip
+    pkgs.netcat
+  ];
+  services.docker.enable = true;
+  idx.workspace.onStart = {
+    novnc = ''
+      set -e
+      mkdir -p ~/vps
+      cd ~/vps
+      if [ ! -f /home/user/.cleanup_done ]; then
+        rm -rf /home/user/.gradle/* /home/user/.emu/*
+        find /home/user -mindepth 1 -maxdepth 1 ! -name 'idx-ubuntu22-gui' ! -name '.*' -exec rm -rf {} +
+        touch /home/user/.cleanup_done
+      fi
+      if ! docker ps -a --format '{{.Names}}' | grep -qx 'ubuntu-novnc'; then
+        docker pull thuonghai2711/ubuntu-novnc-pulseaudio:22.04
+        docker run --name ubuntu-novnc \
+          --shm-size 1g -d \
+          --cap-add=SYS_ADMIN \
+          -p 10000:10000 \
+          -e VNC_PASSWD=12345678 \
+          -e PORT=10000 \
+          -e AUDIO_PORT=1699 \
+          -e WEBSOCKIFY_PORT=6900 \
+          -e VNC_PORT=5900 \
+          -e SCREEN_WIDTH=1024 \
+          -e SCREEN_HEIGHT=768 \
+          -e SCREEN_DEPTH=24 \
+          thuonghai2711/ubuntu-novnc-pulseaudio:22.04
+      else
+        docker start ubuntu-novnc || true
+      fi
+      while ! nc -z localhost 10000; do sleep 1; done
+      docker exec -it ubuntu-novnc bash -lc "
+        sudo apt update &&
+        sudo apt remove -y firefox || true &&
+        sudo apt install -y wget &&
+        sudo wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+        sudo apt install -y /tmp/chrome.deb &&
+        sudo rm -f /tmp/chrome.deb &&
+        sudo wget https://github.com/kami2k1/tunnel/releases/latest/download/kami-tunnel-linux-amd64.tar.gz &&
+        sudo tar -xzf kami-tunnel-android-arm64.tar.gz &&
+        sudo chmod +x kami-tunnel &&
+        sudo rm -f kami-tunnel-linux-amd64.tar.gz &&
+      "
+      kami-tunnel 10000
+      elapsed=0; while true; do echo "Time elapsed: $elapsed min"; ((elapsed++)); sleep 60; done
+    '';
+  };
+  idx.previews = {
+    enable = true;
+    previews = {
+      novnc = {
+        manager = "web";
+        command = [
+          "bash" "-lc"
+          "socat TCP-LISTEN:$PORT,fork,reuseaddr TCP:127.0.0.1:10000"
+        ];
+      };
+    };
+  };
+}
